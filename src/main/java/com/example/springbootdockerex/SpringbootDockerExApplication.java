a36876f5f5a5d64e95a2a4a1d38fe3df0b72cdc6
yun seok <str17gogo@naver.com>
2023-05-16 14:17:51 +0900

    feat: docker 실습 -docker 실습

---
+++ src/main/java/com/example/springbootdockerex/SpringbootDockerExApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerExApplication.class, args);
+    }
+
+}
